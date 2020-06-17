90e241efd6c388b9b0d812658a29b6bf1480babd
timknoop <tim.knoop@hs-furtwangen.de>
2020-06-17 19:16:48 +0200

    aufgabe08update

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -1,21 +1,21 @@
 "use strict";
-var Aufgabe08;
-(function (Aufgabe08) {
-    let submitBut = document.getElementById("submitBut");
-    submitBut.addEventListener("click", communicate);
+var A08Client;
+(function (A08Client) {
+    let submitButton = document.getElementById("submitButton");
+    submitButton.addEventListener("click", communicate);
     async function communicate() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://gis-example.herokuapp.com";
+        let url = "https://timgissose2020.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
         let response = await fetch(url);
         let reply = await response.url;
         console.log(reply);
-        for (let entry of formData) {
+        for (let entry of query) {
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
         }
     }
-})(Aufgabe08 || (Aufgabe08 = {}));
+})(A08Client || (A08Client = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -1,24 +1,25 @@
-namespace Aufgabe08 {
-
-    let submitBut: HTMLInputElement = <HTMLInputElement>document.getElementById("submitBut");
-    submitBut.addEventListener("click", communicate);
+namespace A08Client {
+    
+    let submitButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submitButton");
+    submitButton.addEventListener("click", communicate);
 
     async function communicate(): Promise<void> {
 
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://gis-example.herokuapp.com";
+        let url: string = "https://timgissose2020.herokuapp.com";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
-    
+        
         let response: Response = await fetch(url);
         let reply: string = await response.url;
 
         console.log(reply);
 
-        for (let entry of formData) { 
+        for (let entry of query) {
             console.log(entry);
-            console.log("name: " + entry [0]);
-            console.log( "value: " + entry[1]);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
         }
-    } 
+    }
+    
 }
